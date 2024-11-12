7fe71bc4702b6bd6230ab931b7770f72b076ab04
Malcom Otieno <malcolmotieno16@gmail.com>
2024-11-12 11:17:02 +0300

    efficiency

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -2,15 +2,21 @@ import { Fragment } from "react";
 
 function ListGroup() {
   
-  const items = ["Tokyo", "London", "Sydney", "New York", "San Francisco", "Beijing"];
+  let items = ["Tokyo", "London", "Sydney", "New York", "San Francisco", "Beijing"];
 
   return (
     <Fragment>
       <h1>List</h1>
-      {items.length === 0 ? <p>No items found</p> : null}
+      {items.length === 0 && <p>No items found</p> }
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}>{item}</li>
+        {items.map((item, index) => (
+          <li 
+          className="list-group-item" 
+          key={item} 
+          onClick={() => console.log(item, index)}
+          >
+            {item}
+            </li>
         ))}
       </ul>
     </Fragment>
