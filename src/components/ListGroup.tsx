5f06495546f19e8d18d6d35dd4365e18c975373a
Malcom Otieno <malcolmotieno16@gmail.com>
2024-11-12 12:38:37 +0300

    added highlighting functionality

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,22 +1,27 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 
 function ListGroup() {
-  
-  let items = ["Tokyo", "London", "Sydney", "New York", "San Francisco", "Beijing"];
+  const [items, setItems] = useState(["Tokyo", "London", "Sydney", "New York", "San Francisco", "Beijing"]);
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+  const [name, setName] = useState("Malcom");
+
+  const handleClick = (index: number) => {
+    setSelectedIndex(index);
+  };
 
   return (
     <Fragment>
       <h1>List</h1>
-      {items.length === 0 && <p>No items found</p> }
+      {items.length === 0 && <p>No items found</p>}
       <ul className="list-group">
         {items.map((item, index) => (
-          <li 
-          className="list-group-item" 
-          key={item} 
-          onClick={() => console.log(item, index)}
+          <li
+            className={selectedIndex === index ? "list-group-item active" : "list-group-item"}
+            key={item}
+            onClick={() => handleClick(index)}
           >
             {item}
-            </li>
+          </li>
         ))}
       </ul>
     </Fragment>
